75533bf1c0fbae162c80d4bc92d4d167489fa0d0
Christoph Otter <chris@confio.gmbh>
2024-07-13 15:46:44 +0200

    Use better way to remove elements from vec

--- packages/cw-utils/src/parse_reply.rs
+++ packages/cw-utils/src/parse_reply.rs
@@ -43,7 +43,7 @@ fn parse_protobuf_varint(data: &mut Vec<u8>, field_number: u8) -> Result<usize, 
             field_number
         )));
     }
-    *data = data[i + 1..].to_owned();
+    let _ = data.drain(..=i);
 
     Ok(len as usize) // Gently fall back to the arch's max addressable size
 }
